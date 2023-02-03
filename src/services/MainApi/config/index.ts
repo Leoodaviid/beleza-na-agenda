70b319bda8c751b5b8d704574defe28ff5e87191
Leoodaviid <david_ax7@hotmail.com>
2023-02-03 15:52:23 -0300

    testes

--- src/services/MainApi/config/index.ts
+++ src/services/MainApi/config/index.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const baseApi = axios.create({
   baseURL: "http://132.226.160.248:3000/",
+  headers: {
+    referrerPolicy: "unsafe_url",
+  },
 });
 
 export default baseApi;
