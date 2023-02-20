3166229d288e50440e2850f19e1fbb5c66572509
Leoodaviid <david_ax7@hotmail.com>
2023-02-20 10:15:37 -0300

    Ajustes de cores no caendário

--- src/components/Calendario/Styles.ts
+++ src/components/Calendario/Styles.ts
@@ -14,8 +14,27 @@ export const CalendarioStyle = styled.div`
     font-size: 20px;
   }
 
+  .nice-dates-navigation {
+    color: #742092;
+    font-weight: 700;
+    font-size: 16px;
+    letter-spacing: 1px;
+  }
+  .nice-dates-navigation_previous:before,
+  .nice-dates-navigation_next:before {
+    border-right: 2px solid #742092;
+    border-top: 2px solid #742092;
+    height: 20px;
+    width: 20px;
+  }
+  .nice-dates-navigation_previous:hover:before,
+  .nice-dates-navigation_next:hover:before {
+    border-color: #742092;
+  }
   .nice-dates-week-header_day {
-    color: black;
+    color: #404040;
+    text-transform: uppercase;
+    font-weight: 700;
     font-size: 12px;
   }
 
